9b4c6e6fe911e81be5146d31c81cc5c6d2bfe2dc
bolu <lawalbola125@gmail.com>
2025-01-28 15:14:03 +0200

    registration_form

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'Screens/registration_Screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     title: "RegistrationForm",
+     home: RegistrationScreen(),
+    );
+    
+  }
+}
